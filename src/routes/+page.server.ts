fd845f077cd0cba501e911a0b3267650395a74b1
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-11-23 17:39:17 -0300

    handelbars

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,8 @@
 import { sendEmail } from "$lib/emailService";
 import { fail, type Actions } from "@sveltejs/kit";
 import { error } from "console";
+import html from "./emailTemplate.html?raw"
+import Handlebars from "handlebars";
 
 export const actions: Actions = {
 	default: async ({ request }) => {
@@ -14,15 +16,17 @@ export const actions: Actions = {
 		}
 
 		try {
+			const templeate = Handlebars.compile(html)
 			await sendEmail({
 				from: { name: "Bruno Mollo", address: "something@gmail.com" },
 				to,
 				subject: "Testing email",
-				text: body
+				html: templeate({ body })
 			})
 			return { sended: true, to }
 
 		} catch (err) {
+			console.error(err)
 			throw error(500, 'could not send email')
 		}
 
